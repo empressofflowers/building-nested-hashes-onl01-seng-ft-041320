1017e2447ada7049be21a2137d519044e9413140
Devone Austin <devone.austin@gmail.com>
2020-04-19 16:08:38 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,10 +2,10 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       :patriarch => {}, :matriarch => {}, :hero => {}, :hero_friends => {}
-      }, 
+      },
     :capulet => {
       :patriarch => {}, :matriarch => {}, :heroine => {}, :heroine_friends => {}
       }}
 
-  
+
 end
